261c53ec87f8c3eba18e82676facf7a62fda8ce9
cliffordtm <fdavid.hl3@gmail.com>
2024-12-19 20:54:15 -0300

    atualização, segundo teste

--- tela.py
+++ tela.py
@@ -12,11 +12,13 @@ FPS = 50
 
 Preto = (0,0,0)
 Branco = (255,255,255)
-Azul = (0,0,255)
-centro_tabuleiro = pygame.math.Vector2(ref[0]/2, ref[1]/2.2)
+
 
 
 def game():
+    centro_tabuleiro = pygame.math.Vector2(ref[0]/2, ref[1]/2.2)
+    raio_ref = 30
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -28,31 +30,27 @@ def game():
         
 
         for i in range (10):
-            pygame.draw.circle(tela, Preto, centro_tabuleiro, 30*i, 1)
+            pygame.draw.circle(tela, Preto, centro_tabuleiro, raio_ref*i, 1)
 
-        
-        construir_linhas(tela, Azul, centro_tabuleiro, 24, 300)
-        construir_linhas(tela, Preto, centro_tabuleiro, 12, 300)
-        
+        construir_linhas(tela, Preto, centro_tabuleiro, 12, raio_ref*10)
             
 
         pygame.display.update()
         clock.tick(FPS)
 
-def construir_linhas(tela, cor, posicao_inicial, indice, raio):
+def construir_linhas(tela, cor, ponto_inicial, indice, raio):
     for i in range(indice):
             # o loop for interará para criar os valores dos angulos baseados nessa formula
             # extraindo o valor dos graus e convertendo para radianos
             angulo = (i * 360 / indice) * (math.pi / 180) 
 
             # depois disso é calculado os angulos finais utilizando seno e cosseno do angulo
-            x = centro_tabuleiro.x + (raio - 30) * math.cos(angulo)
-            y = centro_tabuleiro.y + (raio - 30) * math.sin(angulo)
+            x = ponto_inicial.x + (raio - (raio/10)) * math.cos(angulo)
+            y = ponto_inicial.y + (raio - (raio/10)) * math.sin(angulo)
 
             # criada a tupla, podemos adiciona-la no parametro final da função draw
             ponto_final = (x,y)
-            pygame.draw.line(tela, cor, posicao_inicial, ponto_final)
+            pygame.draw.line(tela, cor, ponto_inicial, ponto_final)
 
 game()
-print(centro_tabuleiro)
 pygame.quit()
